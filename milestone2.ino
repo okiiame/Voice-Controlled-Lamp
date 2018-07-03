6159cb15a87678a5a1f0046a67c3210c6cf4e220
Zachary Wang <zachary.wang@gmail.com>
2018-07-03 11:23:25 -0700

    Add files via upload

---
+++ milestone2.ino
@@ -0,0 +1,34 @@
+/*
+ *Turn LED on and off using voice 
+ */
+ 
+int relay_pin = 8;
+String voice;
+void setup(){ 
+  Serial.begin(9600);
+  pinMode(relay_pin,OUTPUT);
+}
+void loop(){
+  //because the bluetooth module sends characters
+  //we have to form the word ourselves using a whileloop
+  while(Serial.available()) {
+    delay(10);
+    char c = Serial.read();
+    if(c == '#'){
+      break;
+    }
+    voice += c;
+  }
+
+  //word testing
+  if(voice.indexOf("on") != -1) {
+    digitalWrite(relay_pin, HIGH);
+    voice = "";
+    delay(1000);
+  }
+  else if(voice.indexOf("off") != -1) {
+    digitalWrite(relay_pin, LOW);
+    voice = "";
+    delay(1000);
+  }
+}
