a2bf30c3c7e63ef9ee4ca6e1348502849f045746
Zachary Wang <zachary.wang@gmail.com>
2018-07-10 09:59:07 -0700

    Updated Code
    Fixed bugs:
    changed indexOf to lastIndexOf
    Fixed some simple mistakes  - base.write() to forearm.write() and others
    changed middle servo's starting angle to 180

--- milestone3.ino
+++ milestone3.ino
@@ -1,7 +1,9 @@
+#include <Brain.h>
+
 #include<Servo.h>
-Servo base; //bottom servo
-Servo upper; //upper arm, middle servo
-Servo forearm; //top servo
+Servo base;
+Servo upper; //upper arm
+Servo forearm;
 
 int relay = 8;
 
@@ -15,14 +17,13 @@ void setup() {
   base.attach(9);
   base.write(10);
   upper.attach(7);
-  upper.write(60);
+  upper.write(180);
   forearm.attach(12);
   forearm.write(0);
   pinMode(relay, OUTPUT);
 }
 
 void loop() {
-  //create the word from the characters sent by the bluetooth module
   while(Serial.available()) {
     delay(10);
     char c = Serial.read();
@@ -36,20 +37,18 @@ void loop() {
   if(voice.length() > 0) { 
      if(voice.indexOf("counterclockwise") != -1) {
        Serial.println(voice);
-       angle = voice.substring(17, voice.indexOf("d"));
+       angle = voice.substring(17, voice.lastIndexOf("d"));
        int moveAngle = base.read() + angle.toInt();
        if(moveAngle>10 && moveAngle<170){
-         for(int pos = base.read(); pos <= moveAngle; pos += 1)  
-         {                                  
+         for(int pos = base.read(); pos <= moveAngle; pos += 1)  {                                  
            base.write(pos);             
            delay(dt);                       
          } 
        }
-       voice = "";
     }
     else if(voice.indexOf("clockwise") != -1) {
       Serial.println(voice);
-      angle = voice.substring(10, voice.indexOf("d"));
+      angle = voice.substring(10, voice.lastIndexOf("d"));
       int moveAngle = base.read() - angle.toInt();
       if(moveAngle>10 && moveAngle<170){
         for(int pos = base.read(); pos >= moveAngle; pos -= 1){                                  
@@ -57,56 +56,56 @@ void loop() {
           delay(dt);                       
         } 
       }
-      voice = "";
     }
-    //now, instead of forward and backward controlling the middle arm, 
-    //down and up controls both, except the middle one only adjusts itself
-    //to make sure the center of mass of the lamp is center
     else if(voice.indexOf("down") != -1) {
       int count = 0;
       Serial.println(voice);
-      angle = voice.substring(8, voice.indexOf("d"));
-      int moveAngle = base.read() + angle.toInt();
+      angle = voice.substring(5, voice.lastIndexOf("d"));
+      Serial.println(angle);
+      int moveAngle = forearm.read() + angle.toInt();
+      Serial.println(moveAngle);
       if(moveAngle>10 && moveAngle<170){
-        for(int pos = base.read(); pos <= moveAngle; pos += 1){                                  
+        for(int pos = forearm.read(); pos <= moveAngle; pos += 1){                           
           forearm.write(pos);
           if(count%5==0){
             upper.write(upper.read()-1);
           }
-          count++;             
-          delay(dt);                       
+          delay(dt);
+          count++;
+          Serial.println(upper.read());                                           
         } 
       }
-      voice = "";
     }         
     else if(voice.indexOf("up") != -1) {
       int count = 0;
       Serial.println(voice);
-      angle = voice.substring(9, voice.indexOf("d"));
-      int moveAngle = base.read() - angle.toInt();
-      if(moveAngle>60 && moveAngle<120){
-        for(int pos = base.read(); pos >= moveAngle; pos -= 1){                                  
+      angle = voice.substring(3, voice.lastIndexOf("d"));
+      Serial.println(angle);
+      int moveAngle = forearm.read() - angle.toInt();
+      Serial.println(moveAngle);
+      if(moveAngle>10 && moveAngle<170){
+        for(int pos = forearm.read(); pos >= moveAngle; pos -= 1){                              
           forearm.write(pos);
           if(count%5==0){
             upper.write(upper.read()+1);
           }
-          count++;             
-          delay(dt);                       
+          delay(dt);
+          count++;
+          Serial.println(upper.read());                               
         } 
       }
-      voice = "";
     }
     else if(voice.indexOf("on") != -1) {
       Serial.println(voice);
       digitalWrite(relay, HIGH);
-      voice = "";
       delay(40);
     }
     else if(voice.indexOf("off") != -1) {
       Serial.println(voice);
       digitalWrite(relay, LOW);
-      voice = "";
       delay(40);
     }
+
+    voice = "";
   }
 }
